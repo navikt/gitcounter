289074eae558860e490d6126f40a6eeb9a872779
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-01-22 10:37:41 +0100

    it works

--- gitcounter.py
+++ gitcounter.py
@@ -5,41 +5,83 @@ import logging
 from git import Repo
 import time
 import shutil
+import os
+import pandas as pd
+import re
 
 
 class GitCounter:
-    def __init__(self):
+    def __init__(self, git_url="https://github.com/navikt/vault-iac", repo_dir="vault-iac"):
         self.logger = logging.getLogger(__name__)
-        self.git_url = "https://github.com/navikt/vault-iac"
-        self.repo_dir = "vault-iac"
+        self.logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+        self.git_url = git_url
+        self.repo_dir = repo_dir
         self.timeout = 60
-        self.sleep =  300
+        self.sleep = 3600
 
     def run(self):
         self.logger.info("starting metrics http server on port 8000")
         start_http_server(8000)
+        gauges = {"postgres": Gauge("databases_postgres", "number of postgres databases"),
+                  "oracle": Gauge("databases_oracle", "number of oracle databases")}
+
         while True:
+            self.clone_repo()
+            counters = self.count_databases()
 
-            #self.clone_GitRepo()
-            #self.count_Databases()
+            self.logger.info(counters)
+            for key, value in counters.items():
+                gauges[key].set(value)
 
+            self.delete_local_repo()
             time.sleep(self.sleep)
 
     def count_databases(self):
-        logging.info("counting databases")
-        postgres = Gauge("databases_postgres", "number of postgres databases", ['team'])
+        self.logger.info("counting databases")
 
-        postgres.set(30)
+        app_yamls = self.get_app_yamls(self.repo_dir + '/terraform/teams')
 
+        df = pd.DataFrame(app_yamls)
+        df.columns = ['path']
 
-def clone_repo(git_url, repo_dir):
-    logging.info("cloning repo")
-    Repo.clone_from(git_url, repo_dir)
+        return {
+            "oracle": df.apply(lambda x: self.has_oracle(x['path']), axis=1).sum(),
+            "postgres": df.apply(lambda x: self.has_pg(x['path']), axis=1).sum()
+        }
 
+    def get_app_yamls(self, rootdir):
+        app_yamls = []
+        for subdir, folders, files in os.walk(rootdir):
+            for file in files:
+                path = os.path.join(subdir, file)
+                if '/apps/' in path:
+                    app_yamls.append(path)
+        return app_yamls
 
-def delete_repo(repo_dir):
-    logging.info("deleting repo")
-    shutil.rmtree(repo_dir)
+    def has_oracle(self, path):
+        file = open(path).read()
+        if 'oracle' in file:
+            return 1
+        return 0
+
+    def has_pg(self, path):
+        file = open(path).read()
+        if 'postgresql' in file:
+            return 1
+        return 0
+
+    def clone_repo(self):
+        self.logger.info("cloning repo")
+        Repo.clone_from(self.git_url, self.repo_dir)
+
+    def delete_local_repo(self):
+        self.logger.info("deleting repo")
+        shutil.rmtree(self.repo_dir)
 
 
 if __name__ == "__main__":
--- test_gitcounter.py
+++ test_gitcounter.py
@@ -1,22 +1,20 @@
-import gitcounter as gc
+import gitcounter
 import os
 import shutil
 
-
-repo_path = "test/test_repo"
+repo_dir = "test/test_repo"
 
 
 def build_test_repo():
-    deep_path = repo_path + "/terraform/teams/team_test/apps"
+    deep_path = repo_dir + "/terraform/teams/team_test/apps"
     os.makedirs(deep_path, exist_ok=True)
     shutil.copyfile("test/resources/testapp.yml", deep_path + "/testapp.yaml")
 
-
-def test_build_repo():
-    assert not os.path.exists(repo_path)
+def test_count_databases():
     build_test_repo()
-    assert os.path.exists(repo_path)
-    gc.delete_repo(repo_path)
-    assert not os.path.exists(repo_path)
+    gc = gitcounter.GitCounter(repo_dir=repo_dir)
 
+    counters = gc.count_databases()
 
+    assert counters["postgres"] == 1
+    assert counters["oracle"] == 1
