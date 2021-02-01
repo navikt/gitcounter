22aa1d0d0acc5f1e51e754bbb9845a7d020ef177
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-02-01 12:38:46 +0100

    added some testcases

--- test_gitcounter.py
+++ test_gitcounter.py
@@ -9,6 +9,8 @@ def build_test_repo():
     deep_path = repo_dir + "/terraform/teams/team_test/apps"
     os.makedirs(deep_path, exist_ok=True)
     shutil.copyfile("test/resources/apps/testapp.yml", deep_path + "/testapp.yaml")
+    shutil.copyfile("test/resources/apps/testapp2.yml", deep_path + "/testapp2.yaml")
+    shutil.copyfile("test/resources/apps/testapp3.yml", deep_path + "/testapp3.yaml")
 
 def test_count_databases():
     build_test_repo()
@@ -16,7 +18,7 @@ def test_count_databases():
 
     counters = gc.count_databases()
 
-    assert counters["postgres"] == 1
-    assert counters["oracle"] == 1
+    assert counters["postgres"] == 2
+    assert counters["oracle"] == 2
 
     shutil.rmtree(repo_dir)
